47296231148334c94cb09edc8e79990049046a0e
Ezamora <edzamo13@gmail.com>
2019-05-17 16:34:38 -0500

    layout pug

--- nodeJs/nodeJSExample/ejemploPrimerServer/src/index.js
+++ nodeJs/nodeJSExample/ejemploPrimerServer/src/index.js
@@ -20,13 +20,37 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 
+//rutas
 app.get('/', (req, res) => {
     //  res.end('Hola Mundo Node Express!.')
     res.render('home', {
         title: 'Curso de Noje.Js',
         message: 'Mensaje primer Layout con variable'
     })
+  /*  res.write(`
+    <h1> Curso De NodeJS</h1>
+    <a href="/temario"> Temario</a> 
+  
+    `)*/
 });
+
+app.get('/temario', (req, res) => {
+    res.render('/temario', {
+        title: 'Curso de Noje.Js',
+        message: 'Temario primer Layout'
+    })
+});
+
+/**
+ * Ejemplo anterio
+ * app.get('/temario/unidad1', (req, res) => {
+    res.write(`
+    <h1> Temario curso NodeJS</h1>
+    <a href="/"> Volver</a> 
+    `)
+});
+ * 
+ */
 
 app.listen('9000', () => {
     console.log('Servidor NodeJS en la ruta http://localhost:9000')
